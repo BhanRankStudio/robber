49aadb86ff72458b9c5fc18bd9fca8b013d8da17
AuJung <nuttapong6280@gmail.com>
2025-05-02 13:09:26 +0700

    Implement QTE system with NPC interaction and configuration files

---
+++ src/shared/configs/NPCConfigs/normal.lua
@@ -0,0 +1,11 @@
+
+return {
+    name = "normal",
+    qteDifficulty = {
+        targetZoneSizeRange = {30, 60},
+        cursorSpeed = 1.0,
+        timeLimit = 2.0,
+        maxAttempts = 5,
+    },
+    itemsDropRate = {},
+}
---
+++ src/shared/configs/NPCConfigs/special.lua
@@ -0,0 +1,12 @@
+
+
+return {
+    name = "special",
+    qteDifficulty = {
+        targetZoneSizeRange = {10, 30},
+        cursorSpeed = 2.0,
+        timeLimit = 1.0,
+        maxAttempts = 1,
+    },
+    itemsDropRate = {},
+}
