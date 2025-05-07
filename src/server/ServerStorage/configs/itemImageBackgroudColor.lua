2becfa5b7bc897798885e4634a1eec5d3f6c3cbe
AuJung <nuttapong6280@gmail.com>
2025-05-07 21:04:41 +0700

    feat: enable blur effect on merchant GUI and add lock color for item images

--- src/server/ServerStorage/configs/itemImageBackgroudColor.lua
+++ src/server/ServerStorage/configs/itemImageBackgroudColor.lua
@@ -7,4 +7,5 @@ return {
     [ItemGrade.RARE] = Color3.fromRGB(0, 0, 255), -- Blue
     [ItemGrade.EPIC] = Color3.fromRGB(255, 0, 255), -- Purple
     [ItemGrade.LEGENDARY] = Color3.fromRGB(255, 215, 0), -- Gold
+    ["lockColor"] = Color3.fromRGB(0,0,0)
 }
